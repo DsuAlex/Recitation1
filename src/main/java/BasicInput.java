648dff9ad4bd07c48476d5979c41078d5e318b9c
alexedwards <agedwards23@students.desu.edu>
2024-01-31 22:14:33 -0500

    good job!

--- src/main/java/BasicInput.java
+++ src/main/java/BasicInput.java
@@ -5,10 +5,30 @@ public class BasicInput {
         Scanner scnr = new Scanner(System.in);
         int userInt = 0;
         double userDouble = 0.0;
+
+
         // TODO Define char and string variables similarly
+        char userChar;
+        String userString;
 
         System.out.println("Enter integer:");
         userInt = scnr.nextInt();
+
+        System.out.println("Enter double:");
+        userDouble = scnr.nextDouble();
+
+        System.out.println("Enter character:");
+        userChar = scnr.next().charAt(0);
+
+        System.out.println("Enter string:");
+        userString = scnr.next();
+
+        System.out.println(userInt + "" + userDouble + "" + userChar + "" + userString);
+        System.out.println(userString + "" + userChar + "" + userDouble + "" + userInt);
+        int intTwo = (int)userDouble;
+        System.out.println(userDouble + " cast to an integer is " + intTwo);
+
+
 
         // TODO (1): Finish reading other items into variables, then output the four values on a single line separated by a space
 
